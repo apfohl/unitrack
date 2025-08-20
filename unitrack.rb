f0727e84cad04d31d5e29650345962b0db5ea437
Andreas Pfohl <andreas.pfohl@uninow.de>
2025-08-20 09:09:14 +0200

    Added brew cask

---
+++ unitrack.rb
@@ -0,0 +1,11 @@
+cask "unitrack" do
+  version "0.2.2"
+  sha256 :no_check
+
+  url "https://github.com/apfohl-uninow/unitrack/releases/download/v0.2.2/unitrack"
+  name "unitrack"
+  desc "Linear time tracker Bubble Tea TUI."
+  homepage "https://github.com/apfohl-uninow/unitrack/"
+
+  binary "unitrack"
+end
